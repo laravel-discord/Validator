40855a3e7a2bea72dee9fc458cf6cb3197fbba31
Charlotte Dunois <github@charuru.moe>
2016-11-11 21:54:36 +0100

    Update example.php

--- example.php
+++ example.php
@@ -2,4 +2,4 @@
 include_once('validator.class.php');
 
 $val = CharlotteDunois\Validation\Validator::make(array('username' => 'CharlotteDunois', 'email' => 'info@charuru', 'regip' => '127.0.0.1', 'sex' => 2), array('username' => 'string|required|min:5|max:75', 'email' => 'email', 'regip' => 'ip', 'sex' => 'int|min:0|max:2'));
-var_dump($val->fails(), $val->getErrors());
+var_dump($val->fails(), $val->errors());
