cdc8a2017464bebc695952a5ae7a1b7f66f744f1
Charlotte Dunois <github@charuru.moe>
2017-10-05 19:02:22 +0200

    Fix³

--- CharlotteDunois/Validation/rules/After.php
+++ CharlotteDunois/Validation/rules/After.php
@@ -12,7 +12,7 @@ namespace CharlotteDunois\Validation\Rule;
 
 class After implements \CharlotteDunois\Validation\ValidationRule {
     function validate($value, $key, $fields, $options, \CharlotteDunois\Validation\Validator $validator) {
-        if(strtotime($options) > strototime($value)) {
+        if(strtotime($options) > strtotime($value)) {
             return array('formvalidator_make_after', array('{0}' => $options));
         }
         
