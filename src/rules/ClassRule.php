b052a189ecde0c1ea5319647a4ac607f85f4185c
Charlotte Dunois <github@charuru.moe>
2018-07-07 21:45:26 +0200

    Add `class` rule

---
+++ src/rules/ClassRule.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Validator
+ * Copyright 2017 Charlotte Dunois, All Rights Reserved
+ *
+ * Docs: https://laravel.com/docs/5.2/validation
+ * Website: https://charuru.moe
+ * License: https://github.com/CharlotteDunois/Validator/blob/master/LICENSE
+**/
+
+namespace CharlotteDunois\Validation\Rule;
+
+class ClassRule implements \CharlotteDunois\Validation\ValidationRule {
+    function validate($value, $key, $fields, $options, $exists, \CharlotteDunois\Validation\Validator $validator) {
+        if($exists === false) {
+            return null;
+        }
+        
+        if(!is_string($value) || !class_exists($value)) {
+            return 'formvalidator_make_class';
+        }
+        
+        $options = ltrim($options, '\\');
+        if(!in_array($options, class_parents($value)) && !in_array($options, class_implements($value))) {
+            return array('formvalidator_make_class_inheritance', array('{0}' => $options));
+        }
+        
+        return true;
+    }
+}
