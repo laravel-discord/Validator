3321b14fc9f9c38030e48ae498c7c5313d531a39
Charlotte Dunois <github@charuru.moe>
2018-07-14 16:19:09 +0200

    Add new folder

---
+++ src/Rules/Uppercase.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Validator
+ * Copyright 2017-2018 Charlotte Dunois, All Rights Reserved
+ *
+ * Website: https://charuru.moe
+ * License: https://github.com/CharlotteDunois/Validator/blob/master/LICENSE
+**/
+
+namespace CharlotteDunois\Validation\Rules;
+
+/**
+ * Name: `uppercase`
+ *
+ * This rule ensures a specific field is all uppercase.
+ */
+class Uppercase implements \CharlotteDunois\Validation\ValidationRule {
+    function validate($value, $key, $fields, $options, $exists, \CharlotteDunois\Validation\Validator $validator) {
+        if(!$exists) {
+            return false;
+        }
+        
+        if(!is_string($value) || mb_strtoupper($value) !== $value) {
+            return 'formvalidator_make_uppercase';
+        }
+        
+        return true;
+    }
+}
